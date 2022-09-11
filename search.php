735d11b5dd55fed3e46892013ee5d097947aa665
avijit <avijit@keylines.net>
2022-09-11 12:38:28 +0530

    index page search design

--- search.php
+++ search.php
@@ -659,6 +659,21 @@ FROM
                 $options2 = get_subCategory_optionsBib($conn);
                 $select_sub = $options2['s'];
 
+                $keywordArr=explode(":",$keyword);        
+                $first=str_replace("Artist", "(Artist) ", $keywordArr[0]);
+                $second=$keywordArr[1];
+                $keyword = $second.' '.$first;
+                
+                $keywordArr=explode(":",$keyword);        
+                $first=str_replace("Author", "(Author) ", $keywordArr[0]);
+                $second=$keywordArr[1];
+                $keyword = $second.' '.$first;
+
+                $keywordArr=explode(":",$keyword);        
+                $first=str_replace("Editor", "(Editor) ", $keywordArr[0]);
+                $second=$keywordArr[1];
+                $keyword = $second.' '.$first;
+
 
                 $search_view = file_get_contents(VIEWS_FOLDER . 'search-result.inc.php');
                 $search = array('{languagelist}', '{journallist}', '{subcategory_list}', '{leftFilter}', '{searchedKeyword}', '{TotalResult}', '{searchList}', '{adv-search-options}');
