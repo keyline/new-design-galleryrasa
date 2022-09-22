a3f926b5b945901b5043824940c1f6df362e069f
keyline <info@keylines.net>
2022-09-22 17:21:53 +0530

    issues was there

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -139,7 +139,7 @@
     define('IMAGES_FOLDER', IMGSRC);
     define('FEATURED_ITEMS', IMGSRC.'featured');
     define('ADMIN_HTML', 'html/');
-	define('PRESS_IMGS', 'press/')
+	define('PRESS_IMGS', 'press/');
     //define('APPS_BASE_PATH', $_SERVER['DOCUMENT_ROOT'] . "/beta/");
 
 
